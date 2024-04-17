1c38cca87d442d1bbb9ecfb3a6f3b33a272499cd
mhassanrahi <m.hassan.rahi@gmail.com>
2024-04-17 13:54:45 +0500

    Added expand article feature

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -4,6 +4,7 @@ import { formatDate } from "../utils";
 function ArticleList({ articles }) {
   const [currentPage, setCurrentPage] = useState(1);
   const articlesPerPage = 5;
+  const [expandedArticles, setExpandedArticles] = useState([]);
 
   const indexOfLastArticle = currentPage * articlesPerPage;
   const indexOfFirstArticle = indexOfLastArticle - articlesPerPage;
@@ -11,16 +12,36 @@ function ArticleList({ articles }) {
     indexOfFirstArticle,
     indexOfLastArticle
   );
+  const toggleExpand = (index) => {
+    setExpandedArticles((prev) =>
+      prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index]
+    );
+  };
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div>
-      <h2 className="text-2xl font-semibold mb-4">Latest Articles</h2>
+      <h2 className="text-2xl font-semibold mb-4">Latest News</h2>
       {currentArticles.map((article, index) => (
         <div key={index} className="mb-4 border border-gray-300 p-4 rounded-md">
           <h2 className="text-lg font-semibold mb-2">{article.title}</h2>
-          <p className="text-gray-700 mb-2">{article.description}</p>
+          <p className="text-gray-700 mb-2">
+            {" "}
+            {article.description && expandedArticles.includes(index)
+              ? article.description
+              : article.description && article.description.length > 300
+              ? article.description.slice(0, 300) + "..."
+              : article.description}
+            {article.description && article.description.length > 300 && (
+              <button
+                className="text-blue-500 hover:underline"
+                onClick={() => toggleExpand(index)}
+              >
+                {expandedArticles.includes(index) ? "Read Less" : "Read More"}
+              </button>
+            )}
+          </p>
           <p className="text-sm text-gray-500">
             Published on: {formatDate(article.publishedAt)}
           </p>
@@ -31,7 +52,7 @@ function ArticleList({ articles }) {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read More
+            Read full article
           </a>
         </div>
       ))}
