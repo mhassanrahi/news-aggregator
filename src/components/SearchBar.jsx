18fe0118fa5abc5b9ea0240a2ea7f458796a17b0
mhassanrahi <m.hassan.rahi@gmail.com>
2024-04-17 13:20:51 +0500

    Revamped the Search Bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import { FaSearch, FaTimes } from "react-icons/fa";
 
-function SearchBar({ onSearch, onClearSearch }) {
+function SearchBar({ onSearch }) {
   const [query, setQuery] = useState("");
 
   const handleSubmit = (event) => {
@@ -10,34 +11,39 @@ function SearchBar({ onSearch, onClearSearch }) {
 
   const handleClear = () => {
     setQuery("");
-    onClearSearch();
+    onSearch("");
   };
 
   return (
-    <div className="mb-4">
-      <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="mb-4">
+      <div className="flex items-center border border-gray-300 rounded-md overflow-hidden">
         <input
           type="text"
           placeholder="Search articles..."
-          className="border border-gray-300 rounded-md px-4 py-2 w-full"
+          className="px-4 py-2 w-full focus:outline-none"
           value={query}
           onChange={(event) => setQuery(event.target.value)}
         />
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded-md ml-2"
-        >
-          Search
-        </button>
-        <button
-          type="button"
-          onClick={handleClear}
-          className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md ml-2"
-        >
-          Clear
-        </button>
-      </form>
-    </div>
+        {query.length > 0 ? (
+          <button
+            type="button"
+            onClick={handleClear}
+            className="px-4 py-2 bg-gray-200 hover:bg-gray-300 rounded mx-2"
+          >
+            <FaTimes />
+          </button>
+        ) : (
+          <button
+            type="submit"
+            className="px-4 py-2 bg-blue-500 text-white hover:bg-blue-600
+            rounded mx-2
+            "
+          >
+            <FaSearch />
+          </button>
+        )}
+      </div>
+    </form>
   );
 }
 
